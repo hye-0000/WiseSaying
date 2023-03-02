0bca6c60cb82fe461db55e83f298ddd6531d8768
hye-0000 <hye_0000@naver.com>
2023-03-02 09:28:33 +0900

    2단계

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -6,8 +6,16 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("==명언 앱==");
         System.out.print("명령)");
-//        Scanner sc = new Scanner(System.in);
-//        sc.nextLine();
+        Scanner sc = new Scanner(System.in);
+        String cmd = sc.nextLine();
+        if(cmd.equals("등록")){
+            System.out.printf("명언 : ");
+            sc.nextLine();
+            System.out.printf("작가 : ");
+            sc.nextLine();
+        }
+        System.out.printf("명령) ");
+        sc.nextLine();
 //        while(true) {
 //            String cmd = sc.nextLine();
 //            if (cmd.equals("등록")) {
