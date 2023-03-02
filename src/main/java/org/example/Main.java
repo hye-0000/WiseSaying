83a76ae5369f935bdce1da0afb1a028b5cd1a5db
hye-0000 <hye_0000@naver.com>
2023-03-02 09:39:08 +0900

    3단계

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -4,26 +4,22 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
+        int id = 0;
         System.out.println("==명언 앱==");
         System.out.print("명령)");
         Scanner sc = new Scanner(System.in);
         String cmd = sc.nextLine();
-        if(cmd.equals("등록")){
-            System.out.printf("명언 : ");
-            sc.nextLine();
-            System.out.printf("작가 : ");
-            sc.nextLine();
+
+        while(cmd.equals("등록")){
+//            if(){
+                id;
+                System.out.printf("명언 : ");
+                sc.nextLine();
+                System.out.printf("작가 : ");
+                sc.nextLine();
+                System.out.println(id + "번 명령이 등록되었습니다.");
+                System.out.print("명령)");
+                cmd = sc.nextLine();
         }
-        System.out.printf("명령) ");
-        sc.nextLine();
-//        while(true) {
-//            String cmd = sc.nextLine();
-//            if (cmd.equals("등록")) {
-//                System.out.printf("명언) ");
-//
-//
-//            }
-//
-//        }
     }
 }
