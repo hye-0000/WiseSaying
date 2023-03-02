dfcace47ea22925403d4c002ebc8ccccd871d871
hye-0000 <hye_0000@naver.com>
2023-03-02 09:22:57 +0900

    1단계

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,21 @@
 package org.example;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("==명언 앱==");
+        System.out.print("명령)");
+//        Scanner sc = new Scanner(System.in);
+//        sc.nextLine();
+//        while(true) {
+//            String cmd = sc.nextLine();
+//            if (cmd.equals("등록")) {
+//                System.out.printf("명언) ");
+//
+//
+//            }
+//
+//        }
     }
 }
